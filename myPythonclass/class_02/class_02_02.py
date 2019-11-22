46fbcc141920a33ea9efc7f55a3f52990e115f94
xuxiaobing-git <xu19890913@163.com>
2019-11-22 23:11:50 +0800

    字符串的替换

---
+++ myPythonclass/class_02/class_02_02.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/11/22 23:06
+# @Author : Administrator
+# @Email : xu19890913@163.com
+# @File : class_02_02.py
+# @Project : xuxiaobingclass
+
+#z字符串的替换 replace()
+str_1="lemon"
+res=str_1.replace("o","@")
+print("替换后的结果：{}".format(res))
