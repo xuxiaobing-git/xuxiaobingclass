9b6ca202fdc84b2e2870f18a7ec17b668b2de419
xuxiaobing-git <xu19890913@163.com>
2019-11-24 20:14:09 +0800

    元组

---
+++ myPythonclass/class_03/class_03_2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/11/24 17:46
+# @Author : Administrator
+# @Email : xu19890913@163.com
+# @File : class_03_2.py
+# @Project : xuxiaobingclass
+
+
+#数据列表
+#标志  []  关键字  list    列表内可以是任意类型
+ #a =[]  空列表
+ #列表的取值：也是根据索引来取值
+a=[1,0.05,"lemonban",(1,2.3,5),[4,5,6]]
+print(a[2])
+print(a[-1])
+#列表的切片
+print(a[0::2])
+print(a[-1][-1])
+#列表的判断语句
+print(2 in a)
+
+
