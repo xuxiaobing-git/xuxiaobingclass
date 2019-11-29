ee86eb1f98a927f885437d409469fc406e47fb70
xuxiaobing-git <xu19890913@163.com>
2019-11-29 10:34:13 +0800

    shanchu

--- webdriver/app/common/read_config.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-
-def read_ini(ini_name, config_x, config_y):  # 根据文件读取ini文件
-	conf = configparser.ConfigParser()
-	conf.read(ini_name, encoding="utf-8-sig")
-	temp = conf.get(config_x, config_y)
-	return temp
--- webdriver/app/data/base_data.py
+++
@@ -1,8 +0,0 @@
-# 账号
-mobile = "18866666666"
-check_code = "1123"
-# 参数，配置文件名称(ini_name)
-id_path = R"E:\webdriver\app\config\id.ini"
-class_path = r"E:\webdriver\app\config\class_name.ini"
-location_path = r"E:\webdriver\app\config\location.ini"
-xpath_path = r"E:\webdriver\app\config\x_path.ini"
