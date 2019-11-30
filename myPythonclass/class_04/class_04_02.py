8db791fa98ed45668c406a55b53809f091fb6c69
xuxiaobing-git <xu19890913@163.com>
2019-11-30 17:10:02 +0800

    22

---
+++ myPythonclass/class_04/class_04_02.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/11/30 14:11
+# @Author : Administrator
+# @Email : xu19890913@163.com
+# @File : class_04_02.py
+# @Project : xuxiaobingclass
+#字典  {}   关键字 dict
+# a={}  空字典     格式   key  :value
+a={"name":"许小兵",}
+print(type(a))
+print(a)
---
+++ myPythonclass/class_04/class_05_01.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/11/30 17:09
+# @Author : Administrator
+# @Email : xu19890913@163.com
+# @File : class_05_01.py
+# @Project : xuxiaobingclass
+a=123
