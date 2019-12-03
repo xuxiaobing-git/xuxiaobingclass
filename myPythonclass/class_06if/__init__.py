26f9c50d7aeb447ffac33181cfdaa0d4e5b03195
xuxiaobing-git <xu19890913@163.com>
2019-12-03 22:35:31 +0800

    条件语句

---
+++ myPythonclass/class_06if/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/12/3 22:26
+# @Author : Administrator
+# @Email : xu19890913@163.com
+# @File : __init__.py.py
+# @Project : xuxiaobingclass
---
+++ myPythonclass/class_06if/class_06_01.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/12/3 22:26
+# @Author : Administrator
+# @Email : xu19890913@163.com
+# @File : class_06_01.py
+# @Project : xuxiaobingclass
+# age=56
+# # if  age>=18:
+# #     print("已经18啦")
+# # else:
+# #     print(age)
+color="999"
+if color=="red":
+    print("红灯停")
+elif color=="green":
+    print("路灯行")
+elif color=="yellow":
+    print("黄灯停一停")
+else:
+    print(color)
