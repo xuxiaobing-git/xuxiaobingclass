24bf9bc68a6b99c8d8c4f8ff216faab719b0af35
xuxiaobing-git <xu19890913@163.com>
2020-06-02 21:59:05 +0800

    Default Changelist0

--- myPythonclass/class_03/class_03_01.py
+++ myPythonclass/class_03/class_03_01.py
@@ -8,6 +8,7 @@
 # str_1="xuxiaobing"
 # str_2=str_1.replace("ing","m")
 # print("修改之后：{}".format(str_2))
+import random
 tuple_1=(1,0.022,"lemon",(1,2,3))
 #元组里面可以是任何类型
 #print(type(tuple_1))
@@ -18,6 +19,10 @@ print(tuple_1[1::2])
 #元组值一旦确定就无法  增  删  改
 #元组可以使用判断元素
 print(1 in tuple_1)
+
+
+a=random.randint(1,100)
+print(a)
 
 
 
