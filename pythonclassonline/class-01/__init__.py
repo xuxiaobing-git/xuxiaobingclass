a3e489cf3c13a872480c2e8f966f2b4c4d59612c
xuxiaobing-git <xu19890913@163.com>
2020-06-02 22:23:16 +0800

    网课学习

---
+++ pythonclassonline/class-01/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/6/2 22:17
+# @Author : Administrator
+# @Email : xu19890913@163.com
+# @File : __init__.py
+# @Project : xuxiaobingclass
---
+++ pythonclassonline/class-01/string-01.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/6/2 22:18
+# @Author : Administrator
+# @Email : xu19890913@163.com
+# @File : string-01
+# @Project : xuxiaobingclass
+
+str1 = "python1"
+str2 = "python2"
+str5 = str1 + str2
+print(str5)
