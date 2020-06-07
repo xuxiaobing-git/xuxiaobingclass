f992d03c2bcb83abe6c8e896606c81a6a32e14dc
xuxiaobing-git <xu19890913@163.com>
2020-06-07 11:22:37 +0800

    字符串的分割

--- pythonclassonline/class-01/string-01.py
+++ pythonclassonline/class-01/string-01.py
@@ -5,7 +5,15 @@
 # @File : string-01
 # @Project : xuxiaobingclass
 
-str1 = "python1"
-str2 = "python2"
-str5 = str1 + str2
-print(str5)
+# str1 = "python1"
+# str2 = "python2"
+# str5 = str1 + str2
+# str6 = str2.split("y")
+# print(str5)
+# print(str6)
+str2 ="aa bb c  7  jj  aa  ll"
+res1 = str2.split(" ")
+print(res1)
+res2="".join(res1)
+print(res2)
+
