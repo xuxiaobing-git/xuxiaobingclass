d21263ac95997680681a0815d11af7f20205243d
xuxiaobing-git <xu19890913@163.com>
2020-06-11 21:41:46 +0800

    登录案例

--- myPythonclass/class_06if/class_06_01.py
+++ myPythonclass/class_06if/class_06_01.py
@@ -12,13 +12,25 @@
 """
 if  条件判断语句
 """
-color=input("请输入红绿灯颜色")
-#color="yellow"
-if color=="red":
-    print("红灯停")
-elif color=="green":
-    print("绿灯行")
-elif color=="yellow":
-    print("黄灯停一停")
+# color=input("请输入红绿灯颜色")
+# #color="yellow"
+# if color=="red":
+#     print("红灯停")
+# elif color=="green":
+#     print("绿灯行")
+# elif color=="yellow":
+#     print("黄灯停一停")
+# else:
+#     print("无此信号灯")
+
+"""登录小案例"""
+users = {"user":"xuxiaobing","pwd":"123456"}
+user = input("请输入登录名：")
+pwd = input("请输入密码：")
+if user==users.get("user"):
+    if pwd == users.get("pwd"):
+        print("登录成功")
+    else:
+        print("密码不对")
 else:
-    print("无此信号灯")
+    print("账号不正确")
