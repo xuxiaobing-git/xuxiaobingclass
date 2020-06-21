940349dca58c2ef0accb79c0fea6524ebbedcf60
xuxiaobing-git <xu19890913@163.com>
2020-06-21 15:17:07 +0800

    图书管理01

--- myPythonclass/class_10class/Booksmanage.py
+++ myPythonclass/class_10class/Booksmanage.py
@@ -6,6 +6,13 @@
 @Email   : 676308756@qq.com
 @Software: PyCharm
 """
+import time
+
+books = [{"id": "1", "name": "天龙八部", "position": "A01-01B-01"},
+         {"id": "2", "name": "水浒传", "position": "A01-01B-02"},
+         {"id": "3", "name": "西游记", "position": "A01-01B-03"},
+         {"id": "4", "name": "红楼梦", "position": "A01-01B-04"},
+         {"id": "5", "name": "三国演义", "position": "A01-01B-05"}]
 
 
 def print_menu():
@@ -17,9 +24,65 @@ def print_menu():
 
 
 def add_book():
+    # 添加图书
+    # new_book = dict(
+    #     #     id=input("请输入图书编号："),
+    #     #     name=input("请输入图书名称："),
+    #     #     position=input("请输入图书位置：")
+    #     # )
+    new_book = {}  # 创建字典存储图书编号
+    while True:
+        new_book["id"] = input("请输入图书编号")
+        for book in books:  # 遍历所有图书，判断编号是否重复
+            if book["id"] == new_book["id"]:
+                print("已有图书：{}".format(book))
+                print("图书编号不能重复")
+                break
+        else:  # 上述for循环 没找到重复的id   进入else
+            new_book["name"] = input("请输入书名：")
+            new_book["position"] = input("请输入书籍位置")
+            books.append(new_book)
+            print("添加成功")
+            break
 
-# def del_book():
-# def all_book():
+        time.sleep(2)
+
+
+def del_book():  # 删除书籍
+    book_name = input("请输入你要删除的书籍")
+    find_list = []  # 创建一个空列表存储书籍
+    for book in books:  # 遍历所有书籍，查找是否有该书籍
+        if book_name == book["name"]:
+            find_list.append(book)
+
+    if len(find_list) != 0:  # 判断接收的列表是否为空，如果为空说明没有改数据
+        print("一共找到{}本同名书籍,名为：{}".format(len(find_list), book["name"]))
+        for book in find_list:
+            print("编号:{}， 书名：{}，位置：{}".format(book["id"], book["name"], book["position"]))
+        id_num = input("请输入你要删除的书籍编号：")
+        # 根据书籍编号进行删除书籍,
+        for book in find_list:
+            if id_num == book["id"]:
+                books.remove(book)
+                print("删除成功")
+                break
+            else:
+                print("输入的编号有误")
+    else:
+        print("抱歉没有该书籍")
+    time.sleep(2)
+
+
+def all_book():
+    book_count = len(books)
+    if book_count == 0:
+        print("当前系统没有任何书籍，返回菜单主页")
+    else:  # 显示当前所有书籍
+        print("当前共有{}本书籍，所有书籍详细信息如下所示".format(book_count))
+        for book in books:
+            print("编号：{}， 书名：{}， 位置：{}".format(book["id"], book["name"], book["position"]))
+        print("查询完毕，返回主菜单")
+    time.sleep(2)
 
 
 def main2():
@@ -28,7 +91,7 @@ def main2():
         print_menu()  # 打印菜单
         num = input("请输入您的选项")
         if num == "1":
-            add_book() # 添加图书
+            add_book()  # 添加图书
         elif num == "2":
             del_book()  # 删除图书
         elif num == "3":
@@ -37,6 +100,9 @@ def main2():
             # 退出程序
             print("谢谢使用，程序即将关闭")
             break
+        else:
+            print("您选择的有误，请重新选择")
 
 
-main2()
+if __name__ == "__main__":
+    main2()
--- myPythonclass/class_11API/class_11_01.py
+++ myPythonclass/class_11API/class_11_01.py
@@ -4,12 +4,14 @@
 # @Email : xu19890913@163.com
 # @File : class_11_01.py
 # @Project : xuxiaobingclass
-import  requests
-url="http://v6-gw.m.163.com/gentie-web/api/v2/products/a2869674571f77b5a0867c3d71db5856/threads/F9HOLR3U05129QAF/wordGeng?ibc=newsappandriod"
-res_get=requests.get(url)
-print("文章详情页结果",res_get)
+import requests
+
+url = "http://v6-gw.m.163.com/gentie-web/api/v2/products/a2869674571f77b5a0867c3d71db5856/threads/F9HOLR3U05129QAF" \
+      "/wordGeng?ibc=newsappandriod "
+res_get = requests.get(url)
+print("文章详情页结果", res_get)
 # print("get请求的状态码",res_get.status_code)
 # print("get请求的响应报文json格式",res_get.json())
-print("get请求的响应报文text格式",res_get.text)
+print("get请求的响应报文text格式", res_get.text)
 # print("get请求的响应头",res_get.headers)
-print("get请求的请求头",res_get.request.headers)
+print("get请求的请求头", res_get.request.headers)
