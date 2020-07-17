e3e8046b8b7b3dc50ec578dc526b51ac47153247
xuxiaobing-git <xu19890913@163.com>
2020-07-17 22:02:11 +0800

    面向对象

---
+++ pythonclassonline/class03-mianxiangduxiang/class-004-attrset.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/7/17 21:29
+# @Author : Administrator
+# @Email : xu19890913@163.com
+# @File : class-004-attrset
+# @Project : xuxiaobingclass
--- pythonclassonline/class03-mianxiangduxiang/class-03-jicheng.py
+++ pythonclassonline/class03-mianxiangduxiang/class-03-jicheng.py
@@ -9,12 +9,20 @@ class BasePhone(object):
     attr = "打电话"
     def call_phone(self):
         print("这个是打电话的功能")
+    def func(self):
+        print("大哥大的宁外一个功能当板砖用")
 class NewPhone01(BasePhone):
     """这是V2版本的手机：老式手机"""
     def sendinfo(self):
         print("发送短信功能")
     def music(self):
         print("播放音乐")
+    def func(self):
+         # """第一种方式调用父类方法"""
+        #BasePhone.func(self)
+        super().func()
+        """第二种方式调用父类方法"""
+        print("这个手机很炫酷")
 class NewPhone02(NewPhone01):
     """这是V3版本的手机：智能机"""
     def dispaly(self):
@@ -25,5 +33,6 @@ v1 = BasePhone()
 
 #v2版本手机
 v2 = NewPhone01()
+v2.func()
 #v3版本手机
 v3 = NewPhone02()
