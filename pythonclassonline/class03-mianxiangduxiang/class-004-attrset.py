9ec5c72a5d2fd600df623af043caaf65604e3a8c
xuxiaobing-git <xu19890913@163.com>
2020-07-21 22:27:22 +0800

    面向对象2

--- pythonclassonline/class03-mianxiangduxiang/class-004-attrset.py
+++ pythonclassonline/class03-mianxiangduxiang/class-004-attrset.py
@@ -4,3 +4,15 @@
 # @Email : xu19890913@163.com
 # @File : class-004-attrset
 # @Project : xuxiaobingclass
+
+class TestCases(object):
+    def __init__(self,url,data,excepted,actual):
+        self.url=url
+        self.data=data
+        self.excepted=excepted
+        self.actual=actual
+
+case1= TestCases(url="www.baidu.com", data={"user":999}, excepted={"user":11}, actual={"code":11})
+setattr(case1,"method","post")
+res = getattr(case1,"method")
+print(res)
