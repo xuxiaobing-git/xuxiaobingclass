0e2d6b8a4b853d0e88c21549ccd29487fbf3fc86
xuxiaobing-git <xu19890913@163.com>
2020-11-21 23:18:18 +0800

    写个乘法表练练手

---
+++ myPythonclass/class_08for/九九乘法表.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/21 22:48
+# @File    : 九九乘法表.py
+# @Software: PyCharm
+row = 1
+while row <= 9:
+    col = 1
+    while col <= row:
+        print("{0} * {1} = {2}".format(col, row, row * col), end="\t")
+        col += 1
+    # print("{}".format(row))
+    print("")
+    row += 1
